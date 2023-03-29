71573a39ae31d9966f7f8bb8d8c7349ee7c0fece
Daniel Sauer <danieljsauer92@gmail.com>
2023-03-28 21:17:37 -0600

    Create Fizz Buzz

---
+++ code_challenges/spicy.rb
@@ -0,0 +1,20 @@
+num = 0
+while num < 100
+num = (num +1)
+
+    if num % 3 == 0 && num % 5 == 0
+        print "Fizz Buzz"
+
+    elsif num % 5 == 0
+        print "Buzz"
+
+    elsif num % 3 == 0 
+        print "Fizz"
+
+    else
+        print num
+    end 
+
+print "\n"
+
+end 
